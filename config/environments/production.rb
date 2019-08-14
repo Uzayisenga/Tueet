3a6ed51753bd3041a06f7e9bddcd66c0f32de841
Uzayisenga <aminauzayisenga@gmail.com>
2019-08-14 12:20:23 +0200

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
-
+  config.serve_static_assets = true # default is false
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
